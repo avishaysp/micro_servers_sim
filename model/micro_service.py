f29c4472af8da8da2b765fb0ae6a2860ed3fd01c
Avishay Spitzer <avishay.spitzer@nextsilicon.com>
2025-04-20 15:07:21 +0300

    events intead of polling

--- model/micro_service.py
+++ model/micro_service.py
@@ -22,19 +22,16 @@ class MicroService:
             if self.queue.items:
                 subtask = yield self.queue.get()
                 process_start = self.env.now
-                # print(f"{process_start}: server #{self.ms_id}# start processing subtask {subtask.id} from task {subtask.parent_task}")
 
-                # Calculate lognormal parameters
-                sigma = 0.5  # Standard deviation for the underlying normal distribution (adjust as needed for shape)
+                sigma = 0.5
                 mean_service_time = 1.0 / self.exp_mu
                 # mu = ln(mean) - sigma^2 / 2
                 mu = math.log(mean_service_time) - (sigma**2 / 2.0)
 
                 yield self.env.timeout(random.lognormvariate(mu, sigma))
-
                 self.process_time_list[self.ms_id] += (self.env.now - process_start)
-                # print(f"{self.env.now}: server #{self.ms_id}# done processing subtask {subtask.id} from task {subtask.parent_task}")
                 yield self.aggregator.queue.put(subtask)
-            else:
-                yield self.env.timeout(0.01)
+
+
+
 
