3799341baf2f4c7aa02cd1da0de410fc99f4ae0e
Avishay Spitzer <avishay.spitzer@nextsilicon.com>
2025-04-20 15:41:08 +0300

    add least loaded 1024

--- simulation/models.py
+++ simulation/models.py
@@ -4,5 +4,10 @@ models = [
         "mu_list": [1 for _ in range(1024)],
         'lb_method': 'rr',
         'description': 'model_2'
-    }
+    },
+    {
+        "mu_list": [1 for _ in range(1024)],
+        'lb_method': 'll',
+        'description': 'model_3'
+    },
 ]
