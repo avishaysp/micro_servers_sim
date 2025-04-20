dfcf97e2989ffe08597b158562a77dc50168ebdf
Avishay Spitzer <avishay.spitzer@nextsilicon.com>
2025-04-20 15:34:29 +0300

    minor

--- simulation/models.py
+++ simulation/models.py
@@ -1,7 +1,7 @@
 models = [
     {"mu_list": [1024], 'lb_method': 'll', 'description': 'model_1'},
     {
-        "mu_list": [1 for i in range(1024)],
+        "mu_list": [1 for _ in range(1024)],
         'lb_method': 'rr',
         'description': 'model_2'
     }
