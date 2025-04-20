37144078ef3636f3d0cc85a5788940ed45865ec2
Avishay Spitzer <avishay.spitzer@nextsilicon.com>
2025-04-20 15:07:42 +0300

    processes instead of threads

--- simulation/models.py
+++ simulation/models.py
@@ -1,4 +1,8 @@
 models = [
     {"mu_list": [1024], 'lb_method': 'll', 'description': 'model_1'},
-    {"mu_list": [1 for i in range(1024)], 'lb_method': 'rr', 'description': 'model_2'}
+    {
+        "mu_list": [1 for i in range(1024)],
+        'lb_method': 'rr',
+        'description': 'model_2'
+    }
 ]
