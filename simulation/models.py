23d4abd87d8665d7639af66587a75f2d1711a517
Avishay Spitzer <avishay.spitzer@nextsilicon.com>
2025-05-06 11:13:03 +0300

    5 models 👍

--- simulation/models.py
+++ simulation/models.py
@@ -1,3 +1,4 @@
+import numpy as np
 from consts import TASK_SIZE
 
 models = [
@@ -7,13 +8,23 @@ models = [
         'description': 'model_1',
     },
     {
-        "mu_list": [1 for _ in range(TASK_SIZE)],
-        'lb_method': 'rr',
+        "mu_list": [TASK_SIZE // 2, TASK_SIZE // 2],
+        'lb_method': 'll',
         'description': 'model_2',
     },
     {
         "mu_list": [1 for _ in range(TASK_SIZE)],
-        'lb_method': 'll',
+        'lb_method': 'rr',
         'description': 'model_3',
+    },
+    {
+        "mu_list": [1 for _ in range(TASK_SIZE)],
+        'lb_method': 'll',
+        'description': 'model_4',
+    },
+    {
+        "mu_list": [1] + [2 ** i for i in range(int(np.log2(TASK_SIZE)))],
+        'lb_method': 'll',
+        'description': 'model_5',
     },
 ]
